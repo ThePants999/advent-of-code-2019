fe61de751b40de89a3548fd39f4f6eec9851e1b2
Chris Paterson <chris.paterson@metaswitch.com>
2020-03-30 17:54:29 +0100

    Remove autogenned docs

--- day6/src/doc/implementors/core/marker/trait.Sync.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["main"] = [{"text":"impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"main/struct.OrbitalObject.html\" title=\"struct main::OrbitalObject\">OrbitalObject</a>","synthetic":true,"types":["main::OrbitalObject"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- day6/src/doc/implementors/core/marker/trait.Unpin.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["main"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"main/struct.OrbitalObject.html\" title=\"struct main::OrbitalObject\">OrbitalObject</a>","synthetic":true,"types":["main::OrbitalObject"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
