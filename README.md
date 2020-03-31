d4ca7a609daffbad5056311acca3ba4b8f37bc53
Chris Paterson <chris.paterson@metaswitch.com>
2020-04-01 00:58:15 +0100

    Redo day 17 so it's actually the program solving the puzzle :-D

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ My Rust solutions to all [Advent of Code 2019](https://adventofcode.com/2019) pr
 
 My style prioritises being easy to follow, understand and reason about.  So you'll typically see a lot _more_ code here than with some other people's solutions - this isn't the place to come to learn how to write really tight, concise Rust that does a lot in few lines.  With the occasional exception, I generally haven't sacrificed much in the way of performance, though. Day 6 is a good example of this.
 ## Current status
-I've completed the Advent of Code, but only the days with times listed in the table below are currently in a state where they're worth looking at (and 18 takes nearly three minutes to execute, so you probably don't want to look at that one 😆). I'm continuing to work on fixing up the other days.
+Everything's as good as I'm planning to make it, except for day 23 that I'm not done with yet. Most days are worth looking at, but I'd skip days 18 and 20 where I haven't learned enough graph theory to write a performant solution, and day 25 is just I/O between the Intcode computer and human user, no automated gameplay.
 
 Running times (best quartile) on my machine:
 
@@ -25,7 +25,7 @@ Running times (best quartile) on my machine:
 | 14  | 0.5       |
 | 15  | 6         |
 | 16  | 84        |
-| 17  |           |
+| 17  | 3         |
 | 18  | 165978 ( ͡° ͜ʖ ͡°) |
 | 19  | 33        |
 | 20  | ~1700     |
--- day17/src/main.rs
+++ day17/src/main.rs
@@ -3,79 +3,259 @@
 #![allow(clippy::cast_possible_truncation)]
 #![allow(clippy::cast_possible_wrap)]
 
-use std::sync::mpsc::channel;
-use std::thread;
-
 use intcode;
 
 fn main() {
-    let memory = intcode::load_program("day17/input.txt").unwrap_or_else(|err| {
+    let start_time = std::time::Instant::now();
+
+    let mut program = intcode::load_program("day17/input.txt").unwrap_or_else(|err| {
         println!("Could not load input file!\n{:?}", err);
         std::process::exit(1);
     });
 
-    let (in_send, in_recv) = channel();
-    let (out_send, out_recv) = channel();
-    let mut computer = intcode::Computer::new(&memory, in_recv, out_send);
-    thread::spawn(move || { computer.run(); });
+    // Run the Intcode computer in camera mode to build a picture of the scaffold.
+    let picture = intcode::run_computer(&program, &[]);
 
-    let program = String::from("B,B,A,C,A,C,A,C,B,C\nR,4,R,6,R,6,R,4,R,4\nR,6,L,8,R,8\nL,8,R,6,L,10,L,10\nn\n");
-    for c in program.chars() {
-        in_send.send(c as i64).unwrap();
-    }
+    // Parse the picture to generate a list of the intersections (needed for part 1) and a set
+    // of movement instructions to follow it (needed for part 2).
+    let scaffold = Scaffold::generate(&picture);
 
-    let mut display = String::new();
-    while let Ok(input) = out_recv.recv() {
-        if input > 255 {
-            println!("Dust: {}", input);
-        } else {
-            match input as u8 as char {
-                '\n' => {
-                    println!("{}", display);
-                    display = String::new();
-                },
-                c => display.push(c),
-            };    
+    // We've found each intersection twice, so we'll need to halve the result.
+    let part_1_answer = scaffold
+        .intersections
+        .iter()
+        .map(|(row, col)| row * col)
+        .sum::<usize>()
+        / 2;
+
+    // Split the movement instructions into subroutines and a main routine, and feed them into
+    // the Intcode computer in movement mode.
+    program[0] = 2;
+    let logic = MovementLogic::parse(&scaffold.program);
+    let part_2_answer = move_robot(&program, logic);
+
+    println!(
+        "Part 1: {}\nPart 2: {}\nTime: {}ms",
+        part_1_answer,
+        part_2_answer,
+        start_time.elapsed().as_millis()
+    );
+}
+
+// Feed the movement logic into the Intcode computer, and fetch the final output, which is
+// the quantity of dust collected.
+fn move_robot(program: &[i64], logic: MovementLogic) -> i64 {
+    let inputs = logic.into_intcode_inputs();
+    let outputs = intcode::run_computer(program, &inputs);
+    let final_output = outputs.last().unwrap();
+    assert!(*final_output > 255);
+    *final_output
+}
+
+struct MovementLogic {
+    main_routine: String,
+    subroutines: Vec<String>,
+}
+
+impl MovementLogic {
+    fn parse(full_program: &str) -> Self {
+        let mut main_routine = full_program.to_string();
+        let mut subroutines = Vec::new();
+
+        loop {
+            // Find the longest set of instructions that's repeated later.
+            for i in (1..=20).rev() {
+                // Skip over any subroutine calls at the beginning.
+                let start = std::cmp::min(
+                    main_routine.find('L').unwrap(),
+                    main_routine.find('R').unwrap(),
+                );
+                let candidate = &main_routine[start..start + i];
+
+                // Skip anything that contains a subroutine call.
+                if candidate.contains('A') || candidate.contains('B') {
+                    continue;
+                }
+
+                if main_routine[start + i..].contains(candidate) {
+                    let mut subroutine = candidate.to_string();
+                    let subroutine_name = match subroutines.len() {
+                        0 => "A,",
+                        1 => "B,",
+                        2 => "C,",
+                        _ => unreachable!(),
+                    };
+                    main_routine = main_routine.replace(&subroutine, subroutine_name);
+                    subroutine.pop(); // Remove trailing comma
+                    subroutines.push(subroutine);
+                    break;
+                }
+            }
+
+            // If there are no movement instructions left, only subroutine calls, we're done.
+            if !main_routine.contains('L') && !main_routine.contains('R') {
+                break;
+            }
+        }
+        main_routine.pop(); // Remove trailing comma
+
+        Self {
+            main_routine,
+            subroutines,
         }
     }
 
+    fn into_intcode_inputs(self) -> Vec<i64> {
+        let mut inputs = Vec::new();
+        self.main_routine
+            .chars()
+            .for_each(|c| inputs.push(c as i64));
+        inputs.push('\n' as i64);
+        for subroutine in self.subroutines {
+            subroutine.chars().for_each(|c| inputs.push(c as i64));
+            inputs.push('\n' as i64);
+        }
+        inputs.push('n' as i64);
+        inputs.push('\n' as i64);
+        inputs
+    }
+}
 
-    //let dust = out_recv.recv().unwrap();
-    //println!("Dust: {}", dust);
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
 
-    // let mut grid: Vec<Vec<char>> = Vec::new();
-    // let mut current_row = Vec::new();
-    // while let Ok(input) = out_recv.recv() {
-    //     match input as u8 as char {
-    //         '\n' => {
-    //             if current_row.len() > 0 {
-    //                 grid.push(current_row);
-    //                 current_row = Vec::new();
-    //             }
-    //         },
-    //         //'^' | '>' | 'v' | '<' => current_row.push('#'),
-    //         c => current_row.push(c),
-    //     };
-    // }
+impl Direction {
+    fn parse(c: char) -> Self {
+        match c {
+            '^' => Self::Up,
+            'v' => Self::Down,
+            '<' => Self::Left,
+            '>' => Self::Right,
+            _ => unreachable!(),
+        }
+    }
 
-    // let height = grid.len();
-    // let width = grid[0].len();
-    // let mut output = String::new();
-    // let mut alignment = 0;
-    // for (y, col) in grid.iter().enumerate() {
-    //     for (x, c) in col.iter().enumerate() {
-    //         if (*c == '#') &&
-    //             (y > 0 && grid[y - 1][x] == '#') &&
-    //             (y < (height - 1) && grid[y + 1][x] == '#') &&
-    //             (x > 0 && grid[y][x - 1] == '#') &&
-    //             (x < (width - 1) && grid[y][x + 1] == '#') {
-    //                 output.push('O');
-    //                 alignment += x * y;
-    //         } else {
-    //             output.push(*c);
-    //         }
-    //     }
-    //     output.push('\n');
-    // }
-    // println!("{}\nAlignment: {}", output, alignment);
+    fn move_one(
+        &self,
+        row: usize,
+        col: usize,
+        width: usize,
+        height: usize,
+    ) -> Result<(usize, usize), ()> {
+        match self {
+            Self::Up if row > 0 => Ok((row - 1, col)),
+            Self::Down if row < (height - 1) => Ok((row + 1, col)),
+            Self::Left if col > 0 => Ok((row, col - 1)),
+            Self::Right if col < (width - 1) => Ok((row, col + 1)),
+            _ => Err(()),
+        }
+    }
+
+    fn turn_left(&self) -> Self {
+        match self {
+            Self::Up => Self::Left,
+            Self::Left => Self::Down,
+            Self::Down => Self::Right,
+            Self::Right => Self::Up,
+        }
+    }
+
+    fn turn_right(&self) -> Self {
+        match self {
+            Self::Up => Self::Right,
+            Self::Right => Self::Down,
+            Self::Down => Self::Left,
+            Self::Left => Self::Up,
+        }
+    }
+}
+
+struct Scaffold {
+    program: String,
+    intersections: Vec<(usize, usize)>,
+}
+
+#[allow(clippy::filter_map)]
+impl Scaffold {
+    fn generate(picture: &[i64]) -> Self {
+        let start_position = picture
+            .iter()
+            .position(|&num| num == 60 || num == 62 || num == 94 || num == 118) // <, >, ^, v
+            .unwrap();
+        let grid: Vec<Vec<char>> = picture
+            .split(|num| *num == 10) // Split on newlines
+            .filter(|slice| !slice.is_empty()) // Drop blank rows
+            .map(|slice| slice.iter().map(|num| *num as u8 as char).collect()) // Convert to chars
+            .collect();
+        let width = grid[0].len();
+        let height = grid.len();
+        let mut row = start_position / (width + 1); // + 1 because we calculated start_position
+        let mut col = start_position % (width + 1); // before removing newlines
+        let mut dir = Direction::parse(grid[row][col]);
+        let mut program = String::new();
+        let mut intersections = Vec::new();
+
+        loop {
+            let mut distance = 0;
+
+            // Go as far as we can along the scaffold in a straight line.
+            while let Ok((new_row, new_col)) = dir.move_one(row, col, width, height) {
+                if grid[new_row][new_col] == '#' {
+                    // We're about to move forwards. Before we do, check to see if the space
+                    // we're vacating was an intersection.
+                    if distance > 0 {
+                        if let Ok((side_row, side_col)) =
+                            dir.turn_left().move_one(row, col, width, height)
+                        {
+                            if grid[side_row][side_col] == '#' {
+                                intersections.push((row, col));
+                            }
+                        }
+                    }
+
+                    // We're OK to move forwards.
+                    row = new_row;
+                    col = new_col;
+                    distance += 1;
+                } else {
+                    // Hit a corner
+                    break;
+                }
+            }
+
+            // Record how far we moved.
+            if distance > 0 {
+                program += &distance.to_string();
+                program.push(',');
+            }
+
+            // Figure out whether to turn left or right to continue following the scaffold, and
+            // record that too.
+            let left_result = dir.turn_left().move_one(row, col, width, height);
+            let right_result = dir.turn_right().move_one(row, col, width, height);
+            if left_result.is_ok() && grid[left_result.unwrap().0][left_result.unwrap().1] == '#' {
+                dir = dir.turn_left();
+                program.push('L');
+                program.push(',');
+            } else if right_result.is_ok()
+                && grid[right_result.unwrap().0][right_result.unwrap().1] == '#'
+            {
+                dir = dir.turn_right();
+                program.push('R');
+                program.push(',');
+            } else {
+                // Neither turning left nor right finds continued scaffold - we must have reached the end.
+                break;
+            }
+        }
+
+        Self {
+            program,
+            intersections,
+        }
+    }
 }
